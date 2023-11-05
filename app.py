859de1b5d7290db5fc40b07aa37dabbee5f602fd
cynth-s <99991701+cynth-s@users.noreply.github.com>
2023-11-05 23:01:35 +0000

    modifications

--- app.py
+++ app.py
@@ -1,10 +1,44 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
+categories = [
+    'General','Science', 'Technology', 'Sports', 'Politics', 'Business'
+]
+
 @app.route("/")
 def news_web():
-  return render_template('home.html')
+  a = requests.get('https://newsapi.org/v2/top-headlines?country=gb&category=general&apiKey=e5a1ef88285b45af973e9c84a68c050f')
+  data = a.json()
+  #print(data['articles'])
 
-if __name__== '__main__':
-  app.run(host ='0.0.0.0', debug = True)
+  urls = [article['url'] for article in data['articles'][:6]]
+  #print(urls)
+  return render_template('home.html',urls=urls,categories=categories)
+
+
+@app.route("/<string:category>")
+def page(category):
+  if category in categories:
+    a = requests.get('https://newsapi.org/v2/everything?q=' + category +
+                     '&apiKey=e5a1ef88285b45af973e9c84a68c050f')
+    data = a.json()
+  
+    urls = [article['url'] for article in data['articles'][:6]]
+  return render_template('home.html',urls=urls ,categories=categories)
+
+@app.route("/category/<string:category>")
+def country(country):
+  if category in Categories:
+    a = requests.get('https://newsapi.org/v2/everything?q=' + category +
+                     '&apiKey=e5a1ef88285b45af973e9c84a68c050f')
+    data = a.json()
+
+    urls = [article['url'] for article in data['articles'][:6]]
+  return render_template('home.html',urls=urls)
+
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug=True)
